abb8a6f5ef8085ec0c24e442e88c2147c905cbab
Luã <luacal@gmail.com>
2024-05-31 01:08:31 -0300

    Implement a different pattern for useContext, separating the context provider and its related functions on a different file

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,26 @@
-import { useRef, useState } from "react";
+import {useRef} from 'react'
 import { createPortal } from "react-dom";
 import Header from "./components/Header.jsx";
 import Meals from "./components/Meals.jsx";
 import Cart from "./components/Cart.jsx";
 
-import { CartContext } from "./store/shopping-cart-context.jsx";
+import CartContextProvider from "./store/shopping-cart-context.jsx";
 
 function App() {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
   const dialog = useRef();
-
-  const ctxValue = {
-    items: shoppingCart.items,
-    addItemToCart: handleAddToCart,
-    updateItemQuantity: handleUpdateItemQuantity,
-  };
-
-
+  
   function handleOpenCart() {
     dialog.current.open();
   }
-
-  function handleUpdateItemQuantity(mealId, amount) {
-    // alert(amount);
-    setShoppingCart((prev) => {
-      const updatedItems = [...prev.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === mealId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity = amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        ...prev,
-        items: updatedItems,
-      };
-    });
-  }
-
-  function handleAddToCart(meal) {
-
-    const productIndex = shoppingCart.items.findIndex((item) => item.id === meal.id);
-
-      if (productIndex !== -1) {
-        // console.log('hasSameProduct', hasSameProduct);
-        console.log(meal);
-        console.log(meal.quantity);
-        console.log(meal.id);
-        console.log('here', shoppingCart.items[productIndex])
-          
-        ctxValue.updateItemQuantity(meal.id, shoppingCart.items[productIndex].quantity + 1);
-        return
-      }
-
-    
-    setShoppingCart((prev) => {
-      //to do: check if the cart already has the product and increment the quantity
-
-      return {
-        ...prev,
-        items: [
-          {
-            id: meal.id,
-            quantity: 1,
-            name: meal.name,
-            price: meal.price,
-          },
-          ...prev.items,
-        ],
-      };
-    });
-  }
-
   
   return (
-    <CartContext.Provider value={ctxValue}>
+    <CartContextProvider>
       <main>
         {createPortal(<Cart ref={dialog} />, document.getElementById("modal"))}
         <Header onCartOpen={handleOpenCart} />
-        <Meals onAdd={handleAddToCart} />
+        <Meals />
       </main>
-    </CartContext.Provider>
+    </CartContextProvider>
   );
 }
 
--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,12 +1,80 @@
-import {createContext} from 'react';
+import { createContext, useState } from "react";
 
 export const CartContext = createContext({
-  items:[],
+  items: [],
   addItemToCart: () => {},
-  updateItemQuantity: () => {}
+  updateItemQuantity: () => {},
 });
 
-// export default function CartContextProvider () {
+export default function CartContextProvider({ children }) {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
+  
 
-// }
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddToCart,
+    updateItemQuantity: handleUpdateItemQuantity,
+  };
 
+  function handleUpdateItemQuantity(mealId, amount) {
+    // alert(amount);
+    setShoppingCart((prev) => {
+      const updatedItems = [...prev.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === mealId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity = amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        ...prev,
+        items: updatedItems,
+      };
+    });
+  }
+
+  function handleAddToCart(meal) {
+    const productIndex = shoppingCart.items.findIndex(
+      (item) => item.id === meal.id
+    );
+
+    if (productIndex !== -1) {
+      ctxValue.updateItemQuantity(
+        meal.id,
+        shoppingCart.items[productIndex].quantity + 1
+      );
+      return;
+    }
+
+    setShoppingCart((prev) => {
+      return {
+        ...prev,
+        items: [
+          {
+            id: meal.id,
+            quantity: 1,
+            name: meal.name,
+            price: meal.price,
+          },
+          ...prev.items,
+        ],
+      };
+    });
+  }
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
