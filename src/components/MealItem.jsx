c6b40d3785363d5656680d00c7b368167cb65789
Luã <luacal@gmail.com>
2024-06-02 00:27:08 -0300

    Fix issue with the price element style

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -10,11 +10,13 @@ export default function MealItem ({mealData}) {
     <li className="meal-item">
         <article>
           <img src={`http://localhost:3000/${mealData.image}`} alt={mealData.description} />
-          <h3>{mealData.name}</h3>
-          <span className="meal-item-price">${mealData.price}</span>
-          <p className="meal-item-description">
-            {mealData.description}
-          </p>
+          <div>
+            <h3>{mealData.name}</h3>
+            <span className="meal-item-price">${mealData.price}</span>
+            <p className="meal-item-description">
+              {mealData.description}
+            </p>
+          </div>
           <p className="meal-item-actions">
             <button className="button" onClick={()=>ctxCart.addItemToCart(mealData)}>
               Add to Cart
