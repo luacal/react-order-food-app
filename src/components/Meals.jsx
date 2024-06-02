2be56d8ea1db9d1ce144dab94c5766fdacb96431
Luã <luacal@gmail.com>
2024-06-02 00:08:25 -0300

    Implement loading and error states on the meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,35 +1,46 @@
-import { useState, useEffect } from 'react'
-import MealItem from './MealItem.jsx'
+import { useState, useEffect } from "react";
+import MealItem from "./MealItem.jsx";
 
-import {fetchMeals} from '../http.js'
+import { fetchMeals } from "../http.js";
 
-export default function Meals () {
+export default function Meals() {
   const [isFetching, setIsFetching] = useState(false);
   const [mealsData, setMealsData] = useState([]);
-  const [error, setError] = useState('');
-    
-  
+  const [error, setError] = useState("");
+
   useEffect(() => {
     setIsFetching(true);
 
-    async function getMeals () {
-
+    async function getMeals() {
       try {
         const data = await fetchMeals();
         setMealsData(data);
       } catch (error) {
-        setError({message: error.message || 'can not fetch the data'})
+        setError({ message: error.message || "can not fetch the data" });
       }
       setIsFetching(false);
     }
     getMeals();
-  }, [])
+  }, []);
+  console.log(error);
+
+  let content;
+
+  if (!isFetching && error) {
+    content = error.message;
+  } else if (!isFetching && !error && mealsData.length === 0) {
+    content = `There aren't any meal listed.`;
+  } else if (isFetching) {
+    content = `Loading...`;
+  } else if (mealsData.length > 0) {
+    content = mealsData.map((mealData) => (
+      <MealItem key={mealData.id} mealData={mealData} />
+    ));
+  }
 
   return (
-    <ul id="meals" >
-      {mealsData.length > 0 && mealsData.map((mealData) => (
-        <MealItem key={mealData.id} mealData={mealData}/>
-      ))}      
+    <ul id="meals">
+      {content}
     </ul>
-  )
+  );
 }
