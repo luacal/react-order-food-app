eb4727085dd182d7859124f1ac53b0ee20b2a000
Luã <luacal@gmail.com>
2024-06-05 20:25:20 -0300

    Implement reusable input components

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,9 +1,10 @@
 import { useContext } from "react";
-import Modal from './UI/Modal.jsx';
+import Modal from "./UI/Modal.jsx";
 import CartContext from "../store/shoppingCartContext.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
 // import formValidation from "../form-validation.js";
 import Button from "./UI/Button.jsx";
+import Input from "./UI/Input.jsx";
 
 export default function CheckoutForm() {
   const cartCtx = useContext(CartContext);
@@ -19,66 +20,59 @@ export default function CheckoutForm() {
     formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
   }
 
-  function handleOrderSubmission (event) {
+  function handleOrderSubmission(event) {
     e.preventDefault();
-    
-    const  fd = new FormData(event.target);
-    const enteredFullName = fd.get('full-name');
-    const enteredEmail = fd.get('email');
-    const enteredStreet = fd.get('street');
-    const enteredPostalCode = fd.get('postal-code');
-    const enteredCity = fd.get('city');
+
+    const fd = new FormData(event.target);
+    const enteredFullName = fd.get("full-name");
+    const enteredEmail = fd.get("email");
+    const enteredStreet = fd.get("street");
+    const enteredPostalCode = fd.get("postal-code");
+    const enteredCity = fd.get("city");
 
     //or: const data = Object.fromEntries(fd.entries()); -> a data object containing all values
 
-    console.log('fd',fd);
+    console.log("fd", fd);
     // validate
-    const value = 'aaaa';
+    const value = "aaaa";
     // console.log('formValidation', formValidation)
     // console.log(formValidation.isNotEmpty(value));
-    
 
     // send the POST to the API
-
-
   }
 
-  function handleCheckoutClose () {
+  function handleCheckoutClose(event) {
+    event.preventDefault();
     userProgressCtx.hideCheckout();
   }
-  
+
   return (
-      <Modal className="checkout" open={userProgressCtx.progress === 'checkout'}>
-      <h2>Checkout</h2>
-      <p>Total Amount: {formattedTotalPrice}</p>
-      <form onSubmit={(event)=>handleOrderSubmission(event)}>
-        <div className="control">
-          <label htmlFor="full-name">Full Name</label>
-          <input id="full-name" name="full-name" type="text" value="Luã Caldeira Oliveira" />
-        </div>
-        <div className="control">
-          <label htmlFor="email">E-Mail Address</label>
-          <input id="email" name="email" type="email" value="luacal@gmail.com" />
-        </div>
-        <div className="control">
-          <label htmlFor="street">Street</label>
-          <input id="street" name="street" type="text" value="Rua Alice B" />
-        </div>
+    <Modal className="checkout" open={userProgressCtx.progress === "checkout"}>
+
+      <form onSubmit={(event) => handleOrderSubmission(event)}>
+
+        <h2>Checkout</h2>
+        <p>Total Amount: {formattedTotalPrice}</p>
+
+        <Input label="Full Name" id="full-name" type="text"></Input>
+        <Input label="E-mail Address" id="email" type="email"></Input>
+        <Input label="Street" id="street" type="text"></Input>
+        
         <div className="control-row">
-          <div className="control">
-            <label htmlFor="postal-code">Postal Code</label>
-            <input id="postal-code" name="postal-code" type="text" value="96785172"/>
-          </div>
-          <div className="control">
-            <label htmlFor="city">City</label>
-            <input id="city" name="city" type="text" value="Camaquã" />
-          </div>
+          <Input label="Postal Code" id="postal-code" type="text"></Input>
+          <Input label="City" id="city" type="text"></Input>
         </div>
+
         <div className="modal-actions">
-          <Button textOnly={true} onClick={handleCheckoutClose}>Close</Button>
+          <Button
+            textOnly={true}
+            onClick={(event) => handleCheckoutClose(event)}
+          >
+            Close
+          </Button>
           <Button>Submit Order</Button>
         </div>
       </form>
-      </Modal>
+    </Modal>
   );
-};
+}
---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,13 @@
+export default function Input({id,label, ...props}) {
+  return (
+    <div className="control">
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        name={id}
+        required
+        {...props}
+      />
+    </div>
+  );
+}
