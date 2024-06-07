52a0351b176a727050c3a44b06023cba53060113
Luã <luacal@gmail.com>
2024-06-06 23:09:39 -0300

    Implement a custom hook to handle the API requests and showing errors

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -2,13 +2,28 @@ import { useContext } from "react";
 import Modal from "./UI/Modal.jsx";
 import CartContext from "../store/shoppingCartContext.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import useHttp from "./hooks/useHttp.js";
 // import formValidation from "../form-validation.js";
 import Button from "./UI/Button.jsx";
 import Input from "./UI/Input.jsx";
 
+const requestConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
 export default function CheckoutForm() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+
+  const {
+    data,
+    isLoading: isSending,
+    error,
+    sendRequest,
+  } = useHttp("http://localhost:3000/orders/", requestConfig);
 
   let formattedTotalPrice = `$0,00`;
 
@@ -37,34 +52,45 @@ export default function CheckoutForm() {
 
     //or: const data = Object.fromEntries(fd.entries()); -> a data object containing all values
 
-    const customerData = Object.fromEntries(fd.entries())
-
-    fetch('http://localhost:3000/orders', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
+    const customerData = Object.fromEntries(fd.entries());
+    console.log('send')
+    sendRequest(
+      JSON.stringify({
         order: {
           items: cartCtx.items,
-          customer: customerData
+          customer: customerData,
         }
       })
-    })
-
+    );
+    userProgressCtx.hideCheckout();
     // validate
-    
+
     // console.log('formValidation', formValidation)
     // console.log(formValidation.isNotEmpty(value));
 
     // send the POST to the API
   }
 
+  let actions = (
+    <>
+      <Button textOnly={true} onClick={(event) => handleCheckoutClose(event)}>
+        Close
+      </Button>
+      <Button>Submit Order</Button>
+    </>
+  );
+
+  if (isSending) {
+    actions = <span>Sending order data...</span>
+  }
+
   return (
-    <Modal className="checkout" open={userProgressCtx.progress === "checkout"} onClose={handleCheckoutClose}>
-
+    <Modal
+      className="checkout"
+      open={userProgressCtx.progress === "checkout"}
+      onClose={handleCheckoutClose}
+    >
       <form onSubmit={(event) => handleOrderSubmission(event)}>
-
         <h2>Checkout</h2>
         <p>Total Amount: {formattedTotalPrice}</p>
 
@@ -77,15 +103,7 @@ export default function CheckoutForm() {
           <Input label="City" id="city" type="text"></Input>
         </div>
 
-        <div className="modal-actions">
-          <Button
-            textOnly={true}
-            onClick={(event) => handleCheckoutClose(event)}
-          >
-            Close
-          </Button>
-          <Button>Submit Order</Button>
-        </div>
+        <div className="modal-actions">{actions}</div>
       </form>
     </Modal>
   );
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,46 +1,47 @@
-import { useState, useEffect } from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "./hooks/useHttp.js";
+import Error from "./Error.jsx";
 
-import { fetchMeals } from "../http.js";
+const requestConfig = {};
 
 export default function Meals() {
-  const [isFetching, setIsFetching] = useState(false);
-  const [mealsData, setMealsData] = useState([]);
-  const [error, setError] = useState("");
+  const {
+    data: mealsData,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    setIsFetching(true);
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
 
-    async function getMeals() {
-      try {
-        const data = await fetchMeals();
-        setMealsData(data);
-      } catch (error) {
-        setError({ message: error.message || "can not fetch the data" });
-      }
-      setIsFetching(false);
-    }
-    getMeals();
-  }, []);
-  console.log(error);
-
-  let content;
-
-  if (!isFetching && error) {
-    content = error.message;
-  } else if (!isFetching && !error && mealsData.length === 0) {
-    content = `There aren't any meal listed.`;
-  } else if (isFetching) {
-    content = `Loading...`;
-  } else if (mealsData.length > 0) {
-    content = mealsData.map((mealData) => (
-      <MealItem key={mealData.id} mealData={mealData} />
-    ));
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
   }
 
   return (
     <ul id="meals">
-      {content}
+      {mealsData &&
+        mealsData.length > 0 &&
+        mealsData.map((mealData) => (
+          <MealItem key={mealData.id} mealData={mealData} />
+        ))}
     </ul>
   );
 }
+
+// let content;
+
+// if (!isLoading && error) {
+//   content = error.message;
+// } else if (!isLoading && !error && mealsData.length === 0) {
+//   content = `There aren't any meal listed.`;
+// } else if (isLoading) {
+//   content = `Loading...`;
+// } else if (mealsData.length > 0) {
+//   content = mealsData.map((mealData) => (
+//     <MealItem key={mealData.id} mealData={mealData} />
+//   ));
+// }
+
+// return <ul id="meals">{content}</ul>;
